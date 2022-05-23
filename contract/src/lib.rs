221f00263cc0bfb55803c3aaf99ac06bec6b7a77
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-23 20:27:51 +0300

    Simple contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,8 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen, PanicOnDefault};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {}
+
+#[near_bindgen]
+impl Contract {
+	pub fn run(self) {
+		env::log("test".as_bytes());
+	}
 }
