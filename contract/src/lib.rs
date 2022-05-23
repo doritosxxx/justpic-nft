fb4f4645b4a0df49e23d875f5bcf8132062fcf27
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-23 21:20:01 +0300

    Playing with infinite loops

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,13 +1,29 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, PanicOnDefault};
+use near_sdk::{env, near_bindgen};
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct Contract {}
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct Contract {
+	iterations: u128,
+}
 
 #[near_bindgen]
 impl Contract {
-	pub fn run(self) {
-		env::log("test".as_bytes());
+	pub fn run(&mut self) {
+		env::log(format!("Deposit: {}", env::attached_deposit()).as_bytes());
+		self.iterations = 0;
+		loop {
+			self.iterations += 1;
+			if self.iterations == 1 {
+				env::log("1".as_bytes());
+			}
+			if self.iterations == 10000 {
+				env::log("10000".as_bytes());
+			}
+		}
+	}
+
+	pub fn iterations(self) -> u128 {
+		self.iterations
 	}
 }
