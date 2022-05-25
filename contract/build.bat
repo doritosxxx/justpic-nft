4b5f42933d388af981d30bf67ee39e10892cb375
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-25 19:19:05 +0300

    Update build.bat

--- contract/build.bat
+++ contract/build.bat
@@ -3,3 +3,4 @@
 cargo build --workspace --target wasm32-unknown-unknown --release
 :: /Y - forced overwrite
 xcopy %CD%\target\wasm32-unknown-unknown\release\*.wasm %CD%\out\main.wasm /Y
+near dev-deploy
