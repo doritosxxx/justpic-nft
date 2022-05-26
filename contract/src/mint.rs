08fd734da18418979c3a547e0ae031b1ef022c81
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-26 18:14:46 +0300

    Implemented `mint`ing

--- contract/src/mint.rs
+++ contract/src/mint.rs
@@ -1,6 +1,18 @@
-use near_contract_standards::non_fungible_token::TokenId;
-use near_sdk::AccountId;
+use crate::structs::NFTToken::{Token, TokenId};
+use crate::*;
+use near_sdk::{near_bindgen, AccountId};
 
 pub trait NonFungibleTokenMint {
-	fn nft_mint(&self, token_id: TokenId, receiver_id: AccountId);
+	fn nft_mint(&mut self, token_id: TokenId, receiver_id: AccountId);
+}
+
+#[near_bindgen]
+impl NonFungibleTokenMint for Contract {
+	fn nft_mint(&mut self, token_id: TokenId, receiver_id: AccountId) {
+		let token = Token {
+			owner_id: receiver_id,
+			token_id: token_id.clone(),
+		};
+		self.tokens_by_id.insert(&token_id.into(), &token);
+	}
 }
