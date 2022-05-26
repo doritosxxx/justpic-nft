4f19193ad2303dbc7d431da3269c42e6bdff7ee2
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-26 18:14:22 +0300

    Added `nft_token` method implementation

--- contract/src/core.rs
+++ contract/src/core.rs
@@ -1,7 +1,5 @@
-use near_contract_standards::non_fungible_token::Token;
-use near_sdk::AccountId;
-use near_contract_standards::non_fungible_token::TokenId;
-use near_sdk::PromiseOrValue;
+use crate::*;
+use near_sdk::{near_bindgen, AccountId, PromiseOrValue};
 
 pub trait NonFungibleTokenCore {
 	fn nft_transfer(&self, receiver_id: AccountId, token_id: TokenId, memo: Option<String>);
@@ -14,4 +12,25 @@ pub trait NonFungibleTokenCore {
 	) -> PromiseOrValue<bool>;
 
 	fn nft_token(&self, token_id: TokenId) -> Option<Token>;
+}
+
+#[near_bindgen]
+impl NonFungibleTokenCore for Contract {
+	fn nft_transfer(&self, receiver_id: AccountId, token_id: TokenId, memo: Option<String>) {
+		todo!()
+	}
+
+	fn nft_transfer_call(
+		&self,
+		receiver_id: AccountId,
+		token_id: TokenId,
+		memo: Option<String>,
+		msg: String,
+	) -> PromiseOrValue<bool> {
+		todo!()
+	}
+
+	fn nft_token(&self, token_id: TokenId) -> Option<Token> {
+		self.tokens_by_id.get(&token_id)
+	}
 }
