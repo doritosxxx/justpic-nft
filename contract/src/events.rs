f8d7da03faf6db43639ee2c6ca581d01664d8659
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-28 20:36:10 +0300

    Updated serialization schema

--- contract/src/events.rs
+++ contract/src/events.rs
@@ -7,19 +7,17 @@ use std::fmt;
 struct EventLog {
 	standard: String,
 	version: String,
-	event: String,
-	//#[serde(flatten)]
+	#[serde(flatten)]
 	data: LogOption,
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "event", content = "data")]
 #[serde(rename_all = "snake_case")]
 #[serde(crate = "near_sdk::serde")]
-//#[serde(tag = "event", content = "data")]
-#[serde(untagged)]
 enum LogOption {
-	Mint(Vec<MintLog>),
-	Transfer(Vec<TransferLog>),
+	NftMint(Vec<MintLog>),
+	NftTransfer(Vec<TransferLog>),
 }
 
 #[derive(Serialize, Deserialize)]
@@ -49,20 +47,11 @@ impl fmt::Display for EventLog {
 	}
 }
 
-fn get_event_string(data: &LogOption) -> String {
-	let str = match data {
-		LogOption::Mint(_) => "nft_mint",
-		LogOption::Transfer(_) => "nft_transfer",
-	};
-	String::from(str)
-}
-
 impl EventLog {
 	pub fn new(data: LogOption) -> Self {
 		Self {
 			standard: String::from("nep171"),
 			version: String::from("1.0.0"),
-			event: get_event_string(&data),
 			data: data,
 		}
 	}
@@ -71,7 +60,7 @@ impl EventLog {
 #[test]
 fn ser() {
 	//let log = EventLog::new(data: LogOption)
-	let log = EventLog::new(LogOption::Mint(vec![MintLog {
+	let log = EventLog::new(LogOption::NftMint(vec![MintLog {
 		owner_id: String::from("alice.near"),
 		token_ids: vec![String::from("aurora")],
 		memo: None,
