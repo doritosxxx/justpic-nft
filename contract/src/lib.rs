7c4bc4fae44ad2767984e77abb846804784668fb
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-28 18:52:10 +0300

    Update lib.rs

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,5 +1,7 @@
+use near_sdk::collections::UnorderedSet;
 use crate::metadata::NonFungibleTokenMetadata;
 use crate::structs::NFTToken::{Token, TokenId};
+use near_sdk::collections::LookupSet;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupMap};
@@ -9,16 +11,21 @@ use near_sdk::{env, near_bindgen, PanicOnDefault};
 use crate::structs::NFTContractMetadata::NFTContractMetadata;
 
 mod core;
+mod enumeration;
 mod metadata;
 mod mint;
 mod structs;
+mod events;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
 	owner_id: AccountId,
 	metadata: LazyOption<NFTContractMetadata>,
-	tokens_by_id: LookupMap<TokenId, Token>,
+	total_supply: u128,
+
+	owner_by_id: LookupMap<TokenId, AccountId>,
+	owner_list: UnorderedSet<AccountId>,
 }
 
 #[near_bindgen]
@@ -27,8 +34,8 @@ impl Contract {
 	pub fn new(owner_id: AccountId) -> Self {
 		let metadata = NFTContractMetadata {
 			spec: String::from("nft-1.0.0"), // required, essentially a version like "nft-1.0.0"
-			name: String::from("justpic"),   // required, ex. "Mosaics"
-			symbol: String::from("JPIC"),    // required, ex. "MOSIAC"
+			name: String::from("nft-curse"), // required, ex. "Mosaics"
+			symbol: String::from("CURSE"),   // required, ex. "MOSIAC"
 			icon: None,                      // Data URL
 			base_uri: None, // Centralized gateway known to have reliable access to decentralized storage assets referenced by `reference` or `media` URLs
 			reference: None, // URL to a JSON file with more info
@@ -36,28 +43,13 @@ impl Contract {
 		};
 
 		let option: LazyOption<NFTContractMetadata> =
-			LazyOption::new(b"meta".try_to_vec().unwrap(), Some(&metadata));
+			LazyOption::new("meta".as_bytes(), Some(&metadata));
 		Self {
-			metadata: option,
 			owner_id,
-			tokens_by_id: LookupMap::new("byid".as_bytes()),
+			metadata: option,
+			owner_by_id: LookupMap::new("byid".as_bytes()),
+			owner_list: UnorderedSet::new("owners".as_bytes()),
+			total_supply: 0,
 		}
 	}
-
-	pub fn clear_state(&self) {
-		env::storage_remove(b"STATE");
-	}
-
-	pub fn get_int(&self) -> i32 {
-		32
-	}
-}
-
-#[test]
-fn test_get_metadata() {
-	let contract = Contract::new("doritosxxx3.testnet".parse().unwrap());
-
-	let metadata = contract.nft_metadata();
-
-	assert_eq!(String::from("justpic"), metadata.name);
 }
