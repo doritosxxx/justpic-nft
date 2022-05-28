6b3263d71589feee8834ec70bdca508f82248f6b
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-28 18:52:34 +0300

    Update mint.rs

--- contract/src/mint.rs
+++ contract/src/mint.rs
@@ -1,7 +1,7 @@
 use crate::structs::NFTToken::{Token, TokenId};
 use crate::structs::TokenMetadata::TokenMetadata;
 use crate::*;
-use near_sdk::{near_bindgen, AccountId};
+use near_sdk::{near_bindgen, require, AccountId};
 
 pub trait NonFungibleTokenMint {
 	fn nft_mint(&mut self, token_id: TokenId, receiver_id: AccountId);
@@ -10,11 +10,18 @@ pub trait NonFungibleTokenMint {
 #[near_bindgen]
 impl NonFungibleTokenMint for Contract {
 	fn nft_mint(&mut self, token_id: TokenId, receiver_id: AccountId) {
-		let token = Token {
-			owner_id: receiver_id,
-			token_id: token_id.clone(),
-			metadata: TokenMetadata::default(),
-		};
-		self.tokens_by_id.insert(&token_id.into(), &token);
+		require!(
+			!self.owner_by_id.contains_key(&token_id),
+			"Token was already minted"
+		);
+
+		require!(
+			!self.owner_list.contains(&receiver_id),
+			"Can't have more than one token per account"
+		);
+
+		self.owner_by_id.insert(&token_id, &receiver_id);
+		self.owner_list.insert(&receiver_id);
+		self.total_supply += 1;
 	}
 }
