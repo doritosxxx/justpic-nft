90c78e5fc39985dba69a33262d698ff67685344c
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-30 22:04:30 +0300

    Update mint.rs

--- contract/src/mint.rs
+++ contract/src/mint.rs
@@ -11,16 +11,15 @@ pub trait NonFungibleTokenMint {
 impl NonFungibleTokenMint for Contract {
 	fn nft_mint(&mut self, token_id: TokenId, receiver_id: AccountId) {
 		require!(
-			!self.owner_by_id.contains_key(&token_id),
+			!self.owner_by_token_id.contains_key(&token_id),
 			"Token was already minted"
 		);
-
 		require!(
 			!self.owner_list.contains(&receiver_id),
 			"Can't have more than one token per account"
 		);
 
-		self.owner_by_id.insert(&token_id, &receiver_id);
+		self.owner_by_token_id.insert(&token_id, &receiver_id);
 		self.owner_list.insert(&receiver_id);
 		self.total_supply += 1;
 
