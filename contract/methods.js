f4fede0abcc499e90bfe80fdd90890a0281d291c
doritosxxx <georgyvav@ya.ru>
2022-12-05 20:07:28 +0300

    support nft_transfer call

--- contract/methods.js
+++ contract/methods.js
@@ -5,9 +5,21 @@ function serialize_params(obj) {
   return JSON.stringify(obj);
 }
 
+function serialize_args(obj) {
+  return Object.entries(obj)
+    .map(
+      ([key, value]) => `${key.startsWith("--") ? key : "--" + key} ${value}`
+    )
+    .join(" ");
+}
+
 // prettier-ignore
-const create_call = (contract, caller, method) => (params) =>new Promise((resolve, reject) => {
-    const cmd = `near call ${contract} ${method} '${serialize_params(params)}' --accountId ${caller}`;
+const create_call = (contract, caller, method) => (params, args) => new Promise((resolve, reject) => {
+    args = {
+      "--accountId": caller,
+      ...args,
+    }
+    const cmd = `near call ${contract} ${method} '${serialize_params(params)}' ${serialize_args(args)}`;
     console.info(cmd);
 
     const child = exec(cmd);
@@ -16,13 +28,16 @@ const create_call = (contract, caller, method) => (params) =>new Promise((resolv
     child.stdout.on("error", reject);
 });
 
+// prettier-ignore
 function get_methods(contract_address, caller_address) {
   const mint = create_call(contract_address, caller_address, "nft_mint");
   const init = create_call(contract_address, caller_address, "new");
+  const transfer = create_call(contract_address, caller_address, "nft_transfer");
 
   return {
     mint,
     init,
+    transfer,
   };
 }
 
