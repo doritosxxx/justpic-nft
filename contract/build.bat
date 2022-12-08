588e4c74bfc48c0788ba55c35a16e2a3970d92e2
doritosxxx <georgyvav@ya.ru>
2022-12-08 23:03:36 +0300

    remove unused scripts

--- contract/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cargo build --workspace --target wasm32-unknown-unknown --release
-:: /Y - forced overwrite
-xcopy %CD%\target\wasm32-unknown-unknown\release\*.wasm %CD%\out\main.wasm /Y
-near dev-deploy -f out/main.wasm
